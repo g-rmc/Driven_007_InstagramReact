5494499e62b1b06aab1c21adcd77c250f5fe6883
g-rmc <g.rmc3000@gmail.com>
2022-07-16 00:01:47 -0300

    Inicio da componentização dos posts

--- src/Posts.js
+++ src/Posts.js
@@ -1,8 +1,138 @@
 export default function Posts() {
 
+    const posts = [{    perfil:{img:'./assets/img/6_Malevola.jpg', name:'Malévola'},
+                        content:{type:'video', midia1:'./assets/video/Video2.mp4', midia2:'./assets/video/video2.ogg'},
+                        likes:{name:'Hermione', number:'127.582'},
+                        comments:{
+                            comment1:{img:'./assets/img/0_Guilherme.jpg', name:'Guilherme', text:'Eu tava, eu fui! ❤️'},
+                            comment2:{img:'./assets/img/2_Mickey.jpg', name:'Mickey', text:'Que assustador'}
+                        }
+                    },
+                    {   perfil:{img:'', name:''},
+                        content:{type:'', midia1:'', midia2:''},
+                        likes:{name:'', number:''},
+                        comments:{
+                            comment1:{img:'', name:'', text:''},
+                            comment2:{img:'', name:'', text:''}
+                        }
+                    },
+                    {   perfil:{img:'', name:''},
+                        content:{type:'', midia1:'', midia2:''},
+                        likes:{name:'', number:''},
+                        comments:{
+                            comment1:{img:'', name:'', text:''},
+                            comment2:{img:'', name:'', text:''}
+                        }
+                    },
+                    {   perfil:{img:'', name:''},
+                        content:{type:'', midia1:'', midia2:''},
+                        likes:{name:'', number:''},
+                        comments:{
+                            comment1:{img:'', name:'', text:''},
+                            comment2:{img:'', name:'', text:''}
+                        }
+                    },
+    ];
+
+    function Post(props) {
+
+        let content;
+
+        function Video(props) {
+
+            return (<div class="post-video">
+                <video controls autoplay>
+                    <source src={props.content.midia1} type="video/mp4" />
+                    <source src={props.content.midia2} type="video/ogg" />
+                    Seu navegador não suporta vídeos
+                </video>
+            </div>)
+        };
+
+        function Picture(props) {
+
+            return (<div class="post-foto">
+                <img src={props.content.midia1} />
+            </div>)
+        };
+
+        if (props.content.type === 'video'){
+
+            content = Video(props);
+
+        } else {
+
+            content = Picture(props);
+        };
+
+        return (
+
+            <div class="post">
+
+                <div class="post-topo">
+                    <div>
+                        <img src={props.perfil.img} />
+                        <h1>{props.perfil.name}</h1>
+                    </div>
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+
+                {content}
+
+                <div class="post-botoes">
+                    <div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <img src="./assets/img/Flag.png" alt="" />
+                </div>
+
+                <div class="post-curtidas">
+                    <img src="./assets/img/1_Hermione.jpg" />
+                    <p>Curtido por <strong>{props.likes.name}</strong> e <strong>outras {props.likes.number} pessoas</strong></p>
+                </div>
+
+                <div class="post-comentarios">
+
+                    <div>
+                        <div>
+                            <img src={props.comments.comment1.img} />
+                            <h1>{props.comments.comment1.name}</h1>
+                            <p>{props.comments.comment1.text}</p>
+                        </div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                    </div>
+
+                    <div>
+                        <div>
+                            <img src={props.comments.comment2.img} />
+                            <h1>{props.comments.comment2.name}</h1>
+                            <p>{props.comments.comment2.text}</p>
+                        </div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                    </div>
+
+                    <div>
+                        <h2>Ver todos os comentários</h2>
+                    </div>
+
+                    <div>
+                        <input type="text" placeholder="Digite seu comentário..." />
+                        <button>Publicar</button>
+                    </div>
+
+                </div>
+
+            </div>
+        )
+    }
+
     return (
 
         <div class="posts">
+
+            {Post(posts[0])}
 
             <div class="post">
 
