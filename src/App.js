9f4cc7619f45cdd28a3e5b8d6f8227f8fc928572
g-rmc <g.rmc3000@gmail.com>
2022-07-17 12:15:10 -0300

    Evolução do componente App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Head from './Head';
-import Body from './Body';
+import Stories from './Stories';
+import Posts from './Posts';
+import Sidebar from './Sidebar';
 import BottomMobile from './BottomMobile';
 
 export default function App() {
@@ -7,7 +9,13 @@ export default function App() {
     return (
         <div>
             <Head />
-            <Body />
+            <div class="conteudo">
+                <div class="feed">
+                    <Stories />
+                    <Posts />
+                </div>
+                <Sidebar />
+            </div>
             <BottomMobile />
         </div>
     );
